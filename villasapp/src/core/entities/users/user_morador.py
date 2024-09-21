5f98d3bc03712eaa8623d967cdc3d6b9889c407a
Angelo <92029180+Anbfer@users.noreply.github.com>
2024-09-21 16:57:37 -0300

    Refactorizar classe Morador para adicionar atributos de instância

--- villasapp/src/core/entities/users/user_morador.py
+++ villasapp/src/core/entities/users/user_morador.py
@@ -1,4 +1,7 @@
-
-class Morador:
+class Morador:    
     def __init__(self, name: str, cpf: str, apartment_num: str, email:str, contact_num: str) -> None:
-        pass
+        self.name = name
+        self.cpf = cpf
+        self.apartment_num = apartment_num
+        self.email = email
+        self.contact_num = contact_num
