d61fa66d7558651d7f083db1e8d44e7c312d2b1e
Angelo <92029180+Anbfer@users.noreply.github.com>
2024-11-19 23:33:19 -0300

    Refatorar API: remover métodos de cadastro e login de moradores, adicionar novos endpoints para gerenciamento de áreas reserváveis e visitantes, e implementar lógica de deleção de moradores por CPF.

--- src/infraestrutura/api/main_api.py
+++ src/infraestrutura/api/main_api.py
@@ -63,33 +63,6 @@ repositorio_area_reservavel = AreaReservavelRepositorio(connector)
 casos_de_uso_area_reservavel = AreaReservavelCasosDeUso(repositorio_area_reservavel)
 
 
-@app.post("/moradores/cadastro/")
-def criar_morador(morador: MoradorModel):
-    try:
-        senha_criptografada = hash_senha(morador.senha)
-        servico.adicionar_morador(
-            nome=morador.nome,
-            email=morador.email,
-            telefone=morador.telefone,
-            cpf=morador.cpf,
-            data_nascimento=morador.data_nascimento,
-            senha=senha_criptografada
-        )
-        return {"mensagem": "Morador criado com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-@app.post("/moradores/login/")
-def login(morador: LoginModel):
-    try:
-        morador_db = servico.buscar_morador_por_email(morador.email)
-        if not morador_db or not verificar_senha(morador.senha, morador_db.senha.encode('utf-8')):
-            raise HTTPException(status_code=401, detail="Credenciais inválidas")
-        return {"mensagem": "Login realizado com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
 @app.get("/moradores/listar/", response_model=List[MoradorModeloResposta])
 def obter_moradores():
     try:
@@ -105,15 +78,6 @@ def obter_moradores():
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.delete("/moradores/deletar/")
-def deletar_moradores(cpf_list: List[str]):
-    try:
-        for cpf in cpf_list:
-            servico.deletar_morador_por_cpf(cpf)
-        return {"mensagem": "Moradores deletados com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
 @app.get("/moradores/pesquisar/", response_model=List[MoradorModeloResposta])
 def pesquisar_moradores(nome: Optional[str] = None, email: Optional[str] = None, cpf: Optional[str] = None, data_nascimento: Optional[str] = None):
     try:
@@ -126,118 +90,6 @@ def pesquisar_moradores(nome: Optional[str] = None, email: Optional[str] = None,
             data_nascimento=morador.data_nascimento,
             senha=morador.senha
         ) for morador in moradores]
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-# Visitantes
-
-@app.post("/visitantes/cadastro/")
-def criar_visitante(visitante: VisitanteModel):
-    try:
-        servico.adicionar_visitante(
-            nome=visitante.nome,
-            cpf=visitante.cpf,
-            telefone=visitante.telefone,
-            veiculo=visitante.veiculo,
-            data_entrada=visitante.data_entrada,
-            data_saida=visitante.data_saida
-        )
-        return {"mensagem": "Visitante criado com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-@app.get("/visitantes/listar/")
-def obter_visitantes(response_model=List[VisitanteModel]):
-    try:
-        visitantes = servico.obter_visitantes()
-        return [VisitanteModel(
-            nome=visitante.nome,
-            cpf=visitante.cpf,
-            telefone=visitante.telefone,
-            veiculo=visitante.veiculo,
-            data_entrada=visitante.data_entrada,
-            data_saida=visitante.data_saida
-        ) for visitante in visitantes
-        ]
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-# Reserva de áreas comuns    
-    
-@app.post("/reservar/agendar/")
-def criar_reserva(reserva: ReservaModel):
-    try:
-        morador = servico.obter_morador_por_cpf(reserva.cpf_morador)
-        if morador is None:
-            raise HTTPException(status_code=404, detail="Morador não encontrado")
-        
-        area_reservavel = casos_de_uso_area_reservavel.obter_area_reservavel_por_nome_aproximado(reserva.area_reserva)
-        if not area_reservavel.disponivel:
-            raise HTTPException(status_code=400, detail=f"Área não está disponível. Reservada por {area_reservavel.reservado_por} em {area_reservavel.data_reserva}")
-        
-        # Atualiza a disponibilidade da área reservável para false se reservado_por não estiver vazio
-        if reserva.reservado_por:
-            casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(area_reservavel.id, False)
-        
-        servico_reserva.adicionar_reserva(
-            id_morador=morador.id,
-            data_reserva=reserva.data_reserva,
-            area_reserva=reserva.area_reserva
-        )
-        return {"mensagem": "Reserva criada com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-
-@app.get("/reservas/listar/")
-def obter_reservas():
-    try:
-        reserva_servico = ReservaServico(casos_de_uso_reserva)
-        reservas = reserva_servico.obter_reservas()
-        return reservas
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-
-@app.delete("/reserva/cancelar/{id_reserva}")
-def cancelar_reserva(id_reserva: int):
-    try:
-        reserva = servico_reserva.obter_reserva_por_id(id_reserva)
-        if reserva is None:
-            raise HTTPException(status_code=404, detail="Reserva não encontrada")
-
-        # Atualiza a disponibilidade da área reservável para true
-        area_reservavel = casos_de_uso_area_reservavel.obter_area_reservavel_por_nome_aproximado(reserva.local_reserva)
-        casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(area_reservavel.id, True)
-
-        servico_reserva.cancelar_reserva(id_reserva)
-        return {"mensagem": "Reserva cancelada com sucesso"}
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-    
-@app.put("/areas_reservaveis/{nome_area}/disponibilidade/")
-def atualizar_disponibilidade_area_reservavel(nome_area: str, disponibilidade: AtualizarDisponibilidadeModel):
-    try:
-        casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(nome_area, disponibilidade.disponivel)
-        return {"mensagem": "Disponibilidade da área reservável atualizada com sucesso"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-@app.post("/areas_reservaveis/cadastro/")
-def criar_area_reservavel(area_reservavel: AreaReservavelModel):
-    try:
-        casos_de_uso_area_reservavel.adicionar_area_reservavel(
-            disponivel=area_reservavel.disponivel,
-            nome_area=area_reservavel.nome_area,
-            horario_funcionamento=area_reservavel.horario_funcionamento,
-            reservado_por=area_reservavel.reservado_por
-        )
-        return {"mensagem": "Área reservável criada com sucesso"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -279,5 +131,143 @@ def obter_areas_reservaveis_por_disponibilidade(disponibilidade: bool):
             horario_funcionamento=area.horario_funcionamento,
             reservado_por=area.reservado_por if area.reservado_por else None
         ) for area in areas_reservaveis]
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/visitantes/listar/")
+def obter_visitantes(response_model=List[VisitanteModel]):
+    try:
+        visitantes = servico.obter_visitantes()
+        return [VisitanteModel(
+            nome=visitante.nome,
+            cpf=visitante.cpf,
+            telefone=visitante.telefone,
+            veiculo=visitante.veiculo,
+            data_entrada=visitante.data_entrada,
+            data_saida=visitante.data_saida
+        ) for visitante in visitantes
+        ]
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/reservas/listar/")
+def obter_reservas():
+    try:
+        reserva_servico = ReservaServico(casos_de_uso_reserva)
+        reservas = reserva_servico.obter_reservas()
+        return reservas
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/visitantes/cadastro/")
+def criar_visitante(visitante: VisitanteModel):
+    try:
+        servico.adicionar_visitante(
+            nome=visitante.nome,
+            cpf=visitante.cpf,
+            telefone=visitante.telefone,
+            veiculo=visitante.veiculo,
+            data_entrada=visitante.data_entrada,
+            data_saida=visitante.data_saida
+        )
+        return {"mensagem": "Visitante criado com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.post("/areas_reservaveis/cadastro/")
+def criar_area_reservavel(area_reservavel: AreaReservavelModel):
+    try:
+        casos_de_uso_area_reservavel.adicionar_area_reservavel(
+            disponivel=area_reservavel.disponivel,
+            nome_area=area_reservavel.nome_area,
+            horario_funcionamento=area_reservavel.horario_funcionamento,
+            reservado_por=area_reservavel.reservado_por
+        )
+        return {"mensagem": "Área reservável criada com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.post("/reservar/agendar/")
+def criar_reserva(reserva: ReservaModel):
+    try:
+        morador = servico.obter_morador_por_cpf(reserva.cpf_morador)
+        if morador is None:
+            raise HTTPException(status_code=404, detail="Morador não encontrado")
+        
+        area_reservavel = casos_de_uso_area_reservavel.obter_area_reservavel_por_nome_aproximado(reserva.area_reserva)
+        if not area_reservavel.disponivel:
+            raise HTTPException(status_code=400, detail=f"Área não está disponível. Reservada por {area_reservavel.reservado_por} em {area_reservavel.data_reserva}")
+        
+        # Atualiza a disponibilidade da área reservável para false se reservado_por não estiver vazio
+        if reserva.reservado_por:
+            casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(area_reservavel.id, False)
+        
+        servico_reserva.adicionar_reserva(
+            id_morador=morador.id,
+            data_reserva=reserva.data_reserva,
+            area_reserva=reserva.area_reserva
+        )
+        return {"mensagem": "Reserva criada com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/moradores/cadastro/")
+def criar_morador(morador: MoradorModel):
+    try:
+        senha_criptografada = hash_senha(morador.senha)
+        servico.adicionar_morador(
+            nome=morador.nome,
+            email=morador.email,
+            telefone=morador.telefone,
+            cpf=morador.cpf,
+            data_nascimento=morador.data_nascimento,
+            senha=senha_criptografada
+        )
+        return {"mensagem": "Morador criado com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/moradores/login/")
+def login(morador: LoginModel):
+    try:
+        morador_db = servico.buscar_morador_por_email(morador.email)
+        if not morador_db or not verificar_senha(morador.senha, morador_db.senha.encode('utf-8')):
+            raise HTTPException(status_code=401, detail="Credenciais inválidas")
+        return {"mensagem": "Login realizado com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.put("/areas_reservaveis/{nome_area}/disponibilidade/")
+def atualizar_disponibilidade_area_reservavel(nome_area: str, disponibilidade: AtualizarDisponibilidadeModel):
+    try:
+        casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(nome_area, disponibilidade.disponivel)
+        return {"mensagem": "Disponibilidade da área reservável atualizada com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.delete("/reserva/cancelar/{id_reserva}")
+def cancelar_reserva(id_reserva: int):
+    try:
+        reserva = servico_reserva.obter_reserva_por_id(id_reserva)
+        if reserva is None:
+            raise HTTPException(status_code=404, detail="Reserva não encontrada")
+
+        # Atualiza a disponibilidade da área reservável para true
+        area_reservavel = casos_de_uso_area_reservavel.obter_area_reservavel_por_nome_aproximado(reserva.local_reserva)
+        casos_de_uso_area_reservavel.atualizar_disponibilidade_area_reservavel(area_reservavel.id, True)
+
+        servico_reserva.cancelar_reserva(id_reserva)
+        return {"mensagem": "Reserva cancelada com sucesso"}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.delete("/moradores/deletar/")
+def deletar_moradores(cpf_list: List[str]):
+    try:
+        for cpf in cpf_list:
+            servico.deletar_morador_por_cpf(cpf)
+        return {"mensagem": "Moradores deletados com sucesso"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
